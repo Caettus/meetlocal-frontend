de9bccc6136711d04cb19f5b26dcce1b66b857ff
Caettus <57236558+Caettus@users.noreply.github.com>
2023-10-04 10:02:53 +0200

    test to see if CI/CD pipeline works

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App, this is test number 1"/>
 </template>
 
 <script>
