b2efd72d7c4a5b6b82775deaeee31a4adcec8133
Caettus <vincentscheffer2@gmail.com>
2023-11-10 17:52:17 +0100

    EVENTS WORDEN AANGEMAAKT AAAHHH

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,13 @@
               <div class="row mb-3">
                 <div class="col-md-6">
                   <div class="form-floating">
-                    <input type="text" class="form-control" id="nameInput">
+                    <input type="text" class="form-control" id="nameInput" v-model="eventName">
                     <label for="nameInput">Event Name</label>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-floating">
-                    <input type="text" class="form-control" id="locationInput">
+                    <input type="text" class="form-control" id="locationInput" v-model="eventLocation">
                     <label for="locationInput">Location</label>
                   </div>
                 </div>
@@ -42,25 +42,25 @@
               <div class="row mb-3">
                 <div class="col-md-6">
                   <div class="form-floating">
-                    <input type="date" class="form-control" id="dateInput">
+                    <input type="date" class="form-control" id="dateInput" v-model="eventDate">
                     <label for="dateInput">Date</label>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-floating">
-                    <input type="time" class="form-control" id="timeInput">
+                    <input type="time" class="form-control" id="timeInput" v-model="eventTime">
                     <label for="timeInput">Time</label>
                   </div>
                 </div>
               </div>
 
               <div class="form-floating">
-                <textarea class="form-control" id="descriptionInput" rows="5"></textarea>
+                <textarea class="form-control" id="descriptionInput" rows="5" v-model="eventDescription"></textarea>
                 <label for="descriptionInput">Description</label>
               </div>
 
               <div class="form-floating">
-                <select class="form-select" id="categoryInput">
+                <select class="form-select" id="categoryInput" v-model="eventCategory">
                   <option value="" disabled selected>Select a category</option>
                   <option value="category1">Festival</option>
                   <option value="category2">Convention</option>
@@ -68,11 +68,20 @@
                 </select>
                 <label for="categoryInput">Category</label>
               </div>
+
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="organiserInput" v-model="eventOrganiser">
+                  <label for="organiserInput">Event Organiser</label>
+                </div>
+              </div>
+
+
             </div>
 
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Create Event</button>
+              <button type="button" class="btn btn-primary" @click="createNewEvent">Create Event</button>
             </div>
           </div>
         </div>
@@ -83,12 +92,83 @@
 </template>
 
 <script>
+const nominatimApiKey = 'https://nominatim.openstreetmap.org/search';
 export default {
   name: 'App',
+  data() {
+    return {
+      eventName: '',
+      eventLocation: '',
+      eventDate: '',
+      eventTime: '',
+      eventDescription: '',
+      eventCategory: '',
+      eventOrganiser: '',
+    };
+  },
   methods: {
+
     createNewEvent() {
-      // Your logic to handle the creation of a new event goes here
-    }
+      // this.checkLocation();
+
+      const eventData ={
+        GatheringName: this.eventName,
+        GatheringDescription: this.eventDescription,
+        GatheringOrganiser: this.eventOrganiser,
+        GatheringDate: this.eventDate,
+        GatheringTime: this.eventTime,
+        GatheringCategory: this.eventCategory,
+        GatheringLocation: this.eventLocation,
+      };
+
+
+      fetch('https://localhost:7031/api/gathering', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(eventData),
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log('Event created successfully!');
+            this.$router.push('/feed');
+          } else {
+            console.error('An error occurred. Event not created.');
+          }
+        })
+        .catch((error) => {
+          console.error('An error occurred:', error);
+        });
+    },
+    checkLocation() {
+      const completeAddress = `${this.city}, ${this.country}`;
+
+      fetch(`${nominatimApiKey}?q=${completeAddress}&format=json`)
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              console.error('Geocoding failed. Address not found.');
+            }
+          })
+          .then((data) => {
+            if (data.length > 0) {
+              this.latitude = data[0].lat;
+              this.longitude = data[0].lon;
+
+              console.log('Latitude', data[0].lat);
+              console.log('Longitude', data[0].lon);
+
+              this.postLocation();
+            } else {
+              console.error('Geocoding failed. Address not found.');
+            }
+          })
+          .catch((error) => {
+            console.error('An error occurred:', error);
+          });
+    },
   },
 };
 </script>
