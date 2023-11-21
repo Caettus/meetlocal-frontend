c7df490ea0fd86b36632d31dadfa613c9a57d34f
Caettus <vincentscheffer2@gmail.com>
2023-11-21 23:39:01 +0100

    ik haat alles

--- src/components/EventFeed/Feed.vue
+++ src/components/EventFeed/Feed.vue
@@ -1,32 +1,54 @@
 <template>
-    <div class="container">
-        <EventCard v-for="event in events" :key="event.id" :event="event" />
-    </div>
+  <div class="container">
+    <input type="text" v-model="search" placeholder="Search gatherings...">
+    <button class="btn btn-primary" @click="searchGatherings">Search</button>
+    <EventCard v-for="event in filteredEvents" :key="event.id" :event="event" />
+  </div>
 </template>
 
 <script>
-import EventCard from "@/components/EventFeed/EventCard.vue";
-import EventService from "@/services/EventService.js";
+import EventCard from '@/components/EventFeed/EventCard.vue';
+import EventService from '@/services/EventService.js';
 export default {
   name: 'EventFeed',
   components: {
-          EventCard
+    EventCard
   },
   data() {
     return {
-      events: null
+      events: [],
+      search: ''
     };
   },
+  computed: {
+    filteredEvents() {
+      return this.events.filter(event => event.GatheringName && event.GatheringName.includes(this.search));
+    }
+  },
+  methods: {
+    getGatherings() {
+      EventService.getGatherings()
+          .then(response => {
+            console.log(response)
+            this.events = response;
+            console.log(response.events)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    searchGatherings() {
+      EventService.searchGatherings(this.search)
+          .then(response => {
+            this.events = response;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+  },
   created() {
-    EventService.getGatherings()
-      .then(response => {
-        console.log(response)
-        this.events = response;
-        console.log(response.events)
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    this.getGatherings();
   }
 };
 </script>
