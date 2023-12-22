ad9d7b7334ff47d8aa72639f3bef8d154a851ee0
Caettus <57236558+Caettus@users.noreply.github.com>
2023-12-22 15:26:13 +0100

    changed homepage ui

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,94 @@
 <template>
   <div class="app">
+    <nav class="navbar">
+      <button class="meetLocalButton">
+        <router-link class="nav-link" to="/">MeetLocal</router-link>
+      </button>
 
-    <li class="nav-item" v-if="$route.path === '/feed'">
-      <router-link class="nav-link" to="/">Home</router-link>
-    </li>
+      <!-- Conditional rendering of "New Event" button -->
+      <button class="btn btn-primary" @click="createNewEvent" data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="$route.path === '/feed'">New Event</button>
+    </nav>
 
+    <!-- New Event Popup -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Create a new event!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
 
-        <!-- Conditional rendering of "New Event" button -->
-        <button class="btn btn-primary" @click="createNewEvent" data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="$route.path === '/feed'">New Event</button>
-      </div>
-
-      <!-- New Event Popup -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Create a new event!</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-              <div class="row mb-3">
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="nameInput" v-model="eventName">
-                    <label for="nameInput">Event Name</label>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="locationInput" v-model="eventLocation">
-                    <label for="locationInput">Location</label>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row mb-3">
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="date" class="form-control" id="dateInput" v-model="eventDate">
-                    <label for="dateInput">Date</label>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="time" class="form-control" id="timeInput" v-model="eventTime">
-                    <label for="timeInput">Time</label>
-                  </div>
-                </div>
-              </div>
-
-              <div class="form-floating">
-                <textarea class="form-control" id="descriptionInput" rows="5" v-model="eventDescription"></textarea>
-                <label for="descriptionInput">Description</label>
-              </div>
-
-              <div class="form-floating">
-                <select class="form-select" id="categoryInput" v-model="eventCategory">
-                  <option value="" disabled selected>Select a category</option>
-                  <option value="category1">Festival</option>
-                  <option value="category2">Convention</option>
-                  <option value="category3">Open Club Meeting</option>
-                </select>
-                <label for="categoryInput">Category</label>
-              </div>
-
+          <div class="modal-body">
+            <div class="row mb-3">
               <div class="col-md-6">
                 <div class="form-floating">
-                  <input type="text" class="form-control" id="organiserInput" v-model="eventOrganiser">
-                  <label for="organiserInput">Event Organiser</label>
+                  <input type="text" class="form-control" id="nameInput" v-model="eventName">
+                  <label for="nameInput">Event Name</label>
                 </div>
               </div>
-
-
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="locationInput" v-model="eventLocation">
+                  <label for="locationInput">Location</label>
+                </div>
+              </div>
             </div>
 
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary" @click="createNewEvent">Create Event</button>
+            <div class="row mb-3">
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="date" class="form-control" id="dateInput" v-model="eventDate">
+                  <label for="dateInput">Date</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="time" class="form-control" id="timeInput" v-model="eventTime">
+                  <label for="timeInput">Time</label>
+                </div>
+              </div>
             </div>
+
+            <div class="form-floating">
+              <textarea class="form-control" id="descriptionInput" rows="5" v-model="eventDescription"></textarea>
+              <label for="descriptionInput">Description</label>
+            </div>
+
+            <div class="form-floating">
+              <select class="form-select" id="categoryInput" v-model="eventCategory">
+                <option value="" disabled selected>Select a category</option>
+                <option value="category1">Festival</option>
+                <option value="category2">Convention</option>
+                <option value="category3">Open Club Meeting</option>
+              </select>
+              <label for="categoryInput">Category</label>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-floating">
+                <input type="text" class="form-control" id="organiserInput" v-model="eventOrganiser">
+                <label for="organiserInput">Event Organiser</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" @click="createNewEvent">Create Event</button>
           </div>
         </div>
       </div>
-      <!-- New Event Popup END-->
+    </div>
+    <!-- New Event Popup END -->
 
     <router-view></router-view>
-
+  </div>
 </template>
 
+
 <script>
+import "@fontsource/poppins";
+
 const nominatimApiKey = 'https://nominatim.openstreetmap.org/search';
 export default {
   name: 'App',
@@ -169,3 +169,23 @@ export default {
   },
 };
 </script>
+
+
+<style>
+.meetLocalButton {
+  background: none;
+  color: #E3C93C;
+  font-family: Poppins;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  border: none;
+}
+
+.navbar {
+  background: transparent;
+}
+
+
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,42 +1,10 @@
 <template>
-  <div class="container py-5">
-    <div class="row justify-content-center">
-      <div class="col-lg-6">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="https://images.pexels.com/photos/2608517/pexels-photo-2608517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="https://images.pexels.com/photos/1157557/pexels-photo-1157557.jpeg?auto=compress&cs=tinysrgb&w=1600" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="https://images.pexels.com/photos/433452/pexels-photo-433452.jpeg?auto=compress&cs=tinysrgb&w=1600" class="d-block w-100" alt="...">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Vorige</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Volgende</span>
-          </button>
-        </div>
-        <div class="card shadow-lg">
-          <div class="card-body p-4">
-            <h2 class="text-center mb-4">Vind evenementen bij jou in de buurt</h2>
-            <Geolocation />
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="background">
   </div>
+  <div class="geolocation">
+    <Geolocation />
+  </div>
+  
 </template>
 
 <script>
@@ -50,11 +18,19 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  border-radius: 1rem;
+.background {
+  background-image: url('@/assets/backgroundimage.jpeg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  height: 100vh;
+  width: 100vw;
 }
-.carousel-item img {
-  height: 300px;
-  object-fit: cover;
+.geolocation {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
+
